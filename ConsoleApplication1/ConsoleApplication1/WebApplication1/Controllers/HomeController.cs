9dfd4a9424c0b30ef39d3aa770f72cf6d1ff8f55
arbaz-kh <47318938+arbaz-kh@users.noreply.github.com>
2019-03-07 14:53:07 +0500

    MVC POSTBACK Request

---
+++ ConsoleApplication1/ConsoleApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(FormCollection fm)
+        {
+            string name = fm["txtname"];
+            ViewBag.nm = name;
+            return View("Welcome");
+        }
+
+        public ActionResult Welcome()
+        {            
+            return View();
+        }
+    }
+}
